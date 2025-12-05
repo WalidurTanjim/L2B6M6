f692e5579d120108d03e956dccc2e1dc39b3e4d4
WalidurTanjim <mohammadtanjim92@gmail.com>
2025-12-05 11:10:35 +0600

    interface practice done

--- src/type_interface.ts
+++ src/type_interface.ts
@@ -1,41 +1,86 @@
-// type alias
-type UserInfo = {
-    name: string;
-    age: number;
-};
+// // type alias
+// type UserInfo = {
+//     name: string;
+//     age: number;
+// };
 
-type UserRole = {
-    role: 'admin' | 'user';
-}
+// type UserRole = {
+//     role: 'admin' | 'user';
+// }
 
-type UserWithRole = UserInfo & UserRole;
+// type UserWithRole = UserInfo & UserRole;
 
-const newUser: UserWithRole = {
-    name: "Walidur Tanjim",
-    age: 25,
-    role: "admin"
-}
+// const newUser: UserWithRole = {
+//     name: "Walidur Tanjim",
+//     age: 25,
+//     role: "admin"
+// }
+
+
+// // type interface
+// interface NewUserInfo {
+//     name: string;
+//     age: number;
+// }
+
+// interface NewUserRole extends NewUserInfo {
+//     role: 'admin' | 'moderator' | 'user';
+// }
+
+// const NewUserWithRole: NewUserRole = {
+//     name: "Walidur Rahman Tanjim",
+//     age: 25,
+//     role: "moderator"
+// }
+
+
+// // function
+// type AddFunction = (num1: number, num2: number) => number;
+
+// interface AddFunctionInterface {
+//     (num1: number, num2: number): number;
+// }
+
+// const addFunction: AddFunctionInterface = (num1, num2) => {
+//     return num1 * num2;
+// };
+
+
+// // array
+// type FriendsArray = string[];
+
+// interface FriendsArrayInterface {
+//     [index: number]: string;
+// }
+
+// const friendsArray: FriendsArrayInterface = ['A', 'B', 'C', 'D', 'E'];
+
+
+
+
 
 
 // type interface
 interface NewUserInfo {
     name: string;
     age: number;
+    isStudent: boolean;
 }
 
-interface NewUserRole extends NewUserInfo {
+interface NewUserWithRole extends NewUserInfo {
     role: 'admin' | 'moderator' | 'user';
 }
 
-const NewUserWithRole: NewUserRole = {
-    name: "Walidur Rahman Tanjim",
+const SingleUser: NewUserWithRole = {
+    name: 'Walidur Tanjim',
     age: 25,
-    role: "moderator"
+    isStudent: true,
+    role: "user"
 }
 
 
-// function
-type AddFunction = (num1: number, num2: number) => number;
+// interface with function
+type AddFunctionType = (num1: number, num2: number) => number;
 
 interface AddFunctionInterface {
     (num1: number, num2: number): number;
@@ -43,14 +88,15 @@ interface AddFunctionInterface {
 
 const addFunction: AddFunctionInterface = (num1, num2) => {
     return num1 * num2;
-};
+}
 
 
-// array
-type FriendsArray = string[];
+// interface with array
+type StringArrayType = string[];
 
-interface FriendsArrayInterface {
+interface StringArrayInterface {
     [index: number]: string;
 }
 
-const friendsArray: FriendsArrayInterface = ['A', 'B', 'C', 'D', 'E'];
+const stringArray: StringArrayInterface = ['A', 'B', 'C', 'D', 'E'];
+
