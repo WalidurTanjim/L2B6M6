20baa64f6963666d0e1d4fe92f684244e95bdb40
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-01-01 22:06:02 +0600

    Generics with interface

---
+++ dist/genericsWithInterface.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const richDeveloper = {
+    name: "Walidur Tanjim",
+    age: 25,
+    salary: 100000,
+    device: {
+        brand: "ASUS",
+        model: "TUF Gaming A15 FA506N",
+        price: 94000
+    },
+    smartWatch: {
+        AI: true,
+        camera: true,
+        record: true,
+        heartRate: true,
+        time_date: true,
+        stopWatch: true
+    }
+};
+const poorDeveloper = {
+    name: "Walidur Tanjim",
+    age: 25,
+    salary: 65000,
+    device: {
+        brand: "HP",
+        model: "245 G7",
+        price: 42000
+    },
+    smartWatch: {
+        heartRate: true,
+        time_date: true,
+        stopWatch: true
+    },
+    bike: {
+        brand: "Apache",
+        model: "RTR",
+        engineCapacity: "150CC",
+        price: 150000
+    }
+};
