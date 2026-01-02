7c089df9e3b4ac574ca1fac73876c171b7cf0dbf
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-01-02 14:45:58 +0600

    Some comment added about subtype on this conditional type in TS

--- src/conditional_type.ts
+++ src/conditional_type.ts
@@ -7,6 +7,22 @@ type C = A extends undefined ? true : B extends number ? true : false;
 
 
 
+/**
+    * undefined is not a subtype of null & number is not a subtype of number. thats why this is false.
+    * Subtype: Each Dog is a animal. But each animal is not a dog.
+    * That's why 'Dog is a subtype of animal'.
+    * 
+    * 
+    * type Person = {
+    *   name: string;
+    * }
+    * 
+    * type Student = {
+    *   name: string;
+    *   role: number;
+    * }
+    * Here each property of person has student type. That's why "Person type is a subtype of Student type".
+ */ 
 
 
 
